3f96d0a65b4ff74cee11fad9db61b823394729d6
iisangil <iisangil@umich.edu>
2022-04-25 20:57:16 -0400

    updated to process video and then save video at end, includes option to play frame by frame

--- car_prediction.py
+++ car_prediction.py
@@ -1,17 +1,8 @@
-from ast import Break
 import cv2
 import numpy as np
+import sys
 from py import process
 from kalmanfilter import KalmanFilter
-from car_detector import CarDetector
-
-cap = cv2.VideoCapture("cut1.mp4")
-
-# Load detector
-car = CarDetector()
-
-# Load Kalman filter to predict the trajectory
-kf = KalmanFilter()
 
 
 def normalized_cross_correlation(ch1, ch2):
@@ -30,19 +21,16 @@ def normalized_cross_correlation(ch1, ch2):
 # _____________________________________________________
 
 # find best bb in new frame that matches old frame
-# def find_best_bb(old_frame, new_frame, old_upL = (x, y), old_lowR = (x1, y1)):
-
 def find_best_bb(old_frame, new_frame, x, y, x1, y1, gamma = 0.03):
+    ref_img = old_frame[y:y1+1, x:x1+1]
 
-    ref_img = old_frame[y:y1, x:x1]
-
-    H = old_frame.shape[0]
     W = old_frame.shape[1]
+    H = old_frame.shape[0]
 
-    H_bb = y1 - y
     W_bb = x1 - x
+    H_bb = y1 - y
 
-    # find search bounds within 10% of old bb
+    # find search bounds within gamma % of old bb
     low_Xi = max(0, int(x * (1 - gamma)))
     high_Xi = min(W - 1, int(x * (1 + gamma)))
 
@@ -53,22 +41,29 @@ def find_best_bb(old_frame, new_frame, x, y, x1, y1, gamma = 0.03):
     # i, j are upper left, u, v are lower right
 
     best_correlation = 0
-    best_offset = (0, 0, 0, 0)
+    best_offset = (x, y, x1, y1)
 
-    # print("X range:", low_Xi, high_Xi)
-    # print("Y range:", low_Yj, high_Yj)
+    max_x = new_frame.shape[1]
+    max_y = new_frame.shape[0]
 
     for i in range(low_Xi, high_Xi):
         for j in range(low_Yj, high_Yj):
             # resize image to make same with ref, old frame
             dim = (ref_img.shape[1], ref_img.shape[0])
 
+            edge_x = i + W_bb
+            edge_y = j + H_bb
+
+            if edge_x > max_x:
+                edge_x = max_x
+            if edge_y > max_y:
+                edge_y = max_y
+
             try:
-                temp_ref = cv2.resize(new_frame[j:y1, i:x1], dim, interpolation = cv2.INTER_AREA)
+                temp_ref = cv2.resize(new_frame[j:edge_y, i:edge_x], dim, interpolation=cv2.INTER_AREA)
             except:
                 print("error resizing")
             else:
-
                 # compute correlation
                 corr = normalized_cross_correlation(temp_ref, ref_img)
 
@@ -86,13 +81,29 @@ def find_best_bb(old_frame, new_frame, x, y, x1, y1, gamma = 0.03):
 
 
 if __name__ == "__main__":
+    vid_name = input("Enter video file:\n")
+    mode = input("Play frames while processing? y or n\n")
+
+    if mode == "y":
+        playback = True
+    else:
+        playback = False
+
+    cap = cv2.VideoCapture(vid_name)
+    num_frames = int(cap.get(7))
+
+    # Load Kalman filter to predict the trajectory
+    kf = KalmanFilter()
+
     print("Processing video...")
 
     iter = 0
-    x, y, x1, y1 = 330, 700, 810, 1050
-
-    width = abs(x1 - x)
-    height = abs(y1 - y)
+    initial_coordinates = vid_name.split("_")
+    x = int(initial_coordinates[0])
+    y = int(initial_coordinates[1])
+    x1 = int(initial_coordinates[2])
+    y1 = initial_coordinates[3]
+    y1 = int(y1[:y1.find(".")])
 
     processed_frames = []
     coordinates = []
@@ -100,19 +111,20 @@ if __name__ == "__main__":
     while True:
         ret, frame = cap.read()
         if ret is False:
-            print(f"Completed processing {iter} frames.")
+            print(f"Completed processing {iter}/{num_frames} frames.")
             break
 
-        if (iter > 0):
-            x, y, x1, y1 = find_best_bb(old_frame, frame, x, y, x1, y1)              
+        if iter > 0:
+            x, y, x1, y1 = find_best_bb(old_frame, frame, x, y, x1, y1)
 
-            cx = (x + x1) / 2
-            cy = (y + y1) / 2
+            cx = int((x + x1) / 2)
+            cy = int((y + y1) / 2)
+
+            width = abs(x1 - x)
+            height = abs(y1 - y)
 
             predicted = kf.predict(cx, cy)
-            print("PREDICTED", predicted)
-            print("Cs", cx, cy)
-            print("COORDINATES", x, y, x1, y1)
+            # print("PREDICTED", predicted)
 
             px = abs(predicted[0] - width / 2)
             px1 = abs(predicted[0] + width / 2)
@@ -121,45 +133,45 @@ if __name__ == "__main__":
 
             predicted = np.array([px, py, px1, py1])
             coordinates.append(predicted)
-            print("PREDICTED", predicted[0], predicted[1], predicted[2], predicted[3])
 
-
-        if iter % 1 == 0:
-            print(f"frame {iter}")
+        if iter % 7 == 0 and iter != 0:
+            print(f"Processing frame {iter+1}/{num_frames}...")
 
         old_frame = frame
         processed_frames.append(old_frame)
         
-        # cv2.rectangle(frame, (x, y), (x1, y1), (255, 0, 0), 4)
+        if playback:
+            cv2.rectangle(frame, (int(x), int(y)), (int(x1), int(y1)), (255, 0, 0), 4)
 
-        # cv2.imshow("Frame", frame)
-        # key = cv2.waitKey(3000)
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(11)
 
-        # if key == 27:
-        #     break
+            if key == 27:
+                break
         
         iter += 1
     
     processed_frames = np.array(processed_frames)
     coordinates = np.array(coordinates)
     
-    print(coordinates.shape)
+    print("Finished processing video. Saving...")
 
-    cv2.imshow("Frame", processed_frames[0])
-    cv2.waitKey(1)
+    box_frames = []
+    box_frames.append(processed_frames[0])
     for i in range(1, processed_frames.shape[0]):
-        cv2.rectangle(frame, (int(coordinates[i-1, 0]), int(coordinates[i-1, 1])), (int(coordinates[i-1, 2]), int(coordinates[i-1, 3])), (255, 0, 0), 4)
-        cv2.imshow("Frame", processed_frames[i])
+        box = cv2.rectangle(processed_frames[i], (int(coordinates[i-1, 0]), int(coordinates[i-1, 1])), (int(coordinates[i-1, 2]), int(coordinates[i-1, 3])), (255, 0, 0), 4)
+        box_frames.append(box)
+    box_frames = np.array(box_frames)
 
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    fps = int(cap.get(5))
+    cap.release()
 
-    while(True):
-        choice = input("Enter r to replay or any key to quit")
+    output = cv2.VideoWriter(f"output_{vid_name}", cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
 
-        if choice == "r":
-            cv2.imshow("Frame", processed_frames[0])
-            for i in range(1, processed_frames.shape[0]):
-                cv2.rectangle(frame, (int(coordinates[i-1, 0]), int(coordinates[i-1, 1])), (int(coordinates[i-1, 2]), int(coordinates[i-1, 3])), (255, 0, 0), 4)
-                cv2.imshow("Frame", processed_frames[i])
-        else:
-            break
+    for frame in box_frames:
+        output.write(frame)
+    output.release()
 
+    print("Video saved.")
